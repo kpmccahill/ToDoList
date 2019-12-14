1e8a872eff4489f973c764c2f5c7049356a28be6
Kyle McCahill <kylemccahill@gmail.com>
2019-12-14 02:38:16 -0600

    added status bar and messages

--- ui/MainWindow.py
+++ ui/MainWindow.py
@@ -12,10 +12,11 @@ from manager.EntryManager import EntryManager
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout,
                              QTableView, QPushButton, QListWidget, QInputDialog,
-                             QListWidgetItem)
+                             QListWidgetItem, QStatusBar)
 
 
 # TODO: Clean up init and break into methods
+# TODO: Add status and info messages with QLabels <------
 class MainWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__()
@@ -29,8 +30,9 @@ class MainWindow(QWidget):
         vBox = QVBoxLayout()
 
         self.taskView = QListWidget()
+
+        # this will make it so that when an item in the list is clicked it is removed immediately.
         # self.taskView.clicked.connect(self.__removeTask)
-        
 
         hBox = QHBoxLayout()
 
@@ -43,6 +45,8 @@ class MainWindow(QWidget):
         saveButton = QPushButton("Save")
         saveButton.clicked.connect(self.__saveList)
 
+        self.statusBar = QStatusBar()
+
         self.__setStyling()
 
         vBox.addLayout(hBox)
@@ -51,11 +55,12 @@ class MainWindow(QWidget):
         hBox.addWidget(removeButton)
 
         vBox.addWidget(self.taskView)
+        vBox.addWidget(self.statusBar)
 
         self.setLayout(vBox)
 
         self.taskView.show()
-        self.__redraw()
+        self.__redrawTaskView()
 
         self.show()
 
@@ -63,8 +68,14 @@ class MainWindow(QWidget):
     def __setStyling(self):
         font = QtGui.QFont()
         font.setFamily("Arial")
-        font.setPointSize(12)
+        font.setPointSize(11)
         self.taskView.setFont(font)
+
+        statusFont = font
+        statusFont.setPointSize(9)
+        self.statusBar.setFont(font)
+        self.statusBar.setStyleSheet("color: red")
+        self.statusBar.setSizeGripEnabled(False)
 
     """Adds a new entry to the entry manager"""
     def __addTask(self):
@@ -73,22 +84,26 @@ class MainWindow(QWidget):
             date, ok = QInputDialog.getText(self, "Add Task", "Enter the due date: ")
             if ok:
                 self.manager.addEntry(Entry(item, date))
-                self.__redraw()
+                self.__redrawTaskView()
+                self.statusBar.showMessage("Added", MESSAGE_TIME)
 
     """Removes the selected item from the list"""
     def __removeTask(self):
         try:
             self.manager.removeEntry(self.taskView.currentItem().text())
-            self.__redraw()
+            self.__redrawTaskView()
+            self.statusBar.showMessage("Removed", MESSAGE_TIME)
         except Exception:
             logging.error('Error nothing was selected')
+            self.statusBar.showMessage("Error: Nothing was selected", MESSAGE_TIME)
 
     """Saves the list of tasks to the database"""
     def __saveList(self):
         self.manager.saveToDB()
+        self.statusBar.showMessage("Saved", MESSAGE_TIME)
 
     """Redraws the taskView to keep it up to date"""
-    def __redraw(self):
+    def __redrawTaskView(self):
         self.taskView.clear()
         for entry in self.manager.entryList:
             entryString = entry.to_string()
@@ -96,6 +111,10 @@ class MainWindow(QWidget):
             entryString = QListWidgetItem(entryString)
             entryString.setTextAlignment(QtCore.Qt.AlignHCenter)
             self.taskView.addItem(entryString)
+
+
+# CONSTANTS #
+MESSAGE_TIME = 2000
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
